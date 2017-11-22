1e3a46c1cd5fb2f358c316f1dcf7b54cbccd9c41
Nirmal Patel <nirmal.06it37@gmail.com>
2017-11-22 11:46:29 +0000

    ikea chaincode

---
+++ SetupNetwork/chaincode/ikea/read_ledger.go
@@ -0,0 +1,92 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	pb "github.com/hyperledger/fabric/protos/peer"
+)
+
+// ============================================================================================================================
+// Get All Ikea Received
+// ============================================================================================================================
+func getAllIkeaReceived(stub  shim.ChaincodeStubInterface, option string, value string) pb.Response {
+	fmt.Println("getAllIkeaReceived:Looking for All Ikea Received");
+
+	//get the AllAbattoirReceived index
+	allBAsBytes, err := stub.GetState("allIkeaReceivedIds")
+	if err != nil {
+		return shim.Error("Failed to get all Ikea Received")
+	}
+
+	var res AllIkeaReceivedIds
+	err = json.Unmarshal(allBAsBytes, &res)
+	//fmt.Println(allBAsBytes);
+	if err != nil {
+		fmt.Println("Printing Unmarshal error:-");
+		fmt.Println(err);
+		return shim.Error("Failed to Unmarshal all Ikea Received Ids")
+	}
+
+	var allIds AllIkeaReceivedIds
+	var allDetails AllIkeaReceivedDetails
+	var sb IkeaReceived
+	if strings.ToLower(option) == "id" && value != "" {
+		sbAsBytes, err := stub.GetState(value)
+		if err != nil {
+			return shim.Error("Failed to get Ikea Received Number ")
+		}
+		json.Unmarshal(sbAsBytes, &sb)
+		if sb.IkeaReceivedNumber != "" {
+			allDetails.IkeaReceived = append(allDetails.IkeaReceived,sb);	
+		}
+		rabAsBytes, _ := json.Marshal(allDetails)
+		return shim.Success(rabAsBytes)	
+	}
+
+	for i := range res.IkeaReceivedNumbers{
+		sbAsBytes, err := stub.GetState(res.IkeaReceivedNumbers[i])
+		if err != nil {
+			return shim.Error("Failed to get Ikea Received Number ")
+		}		
+		json.Unmarshal(sbAsBytes, &sb)
+
+		if strings.ToLower(option) == "ids" {
+			allIds.IkeaReceivedNumbers = append(allIds.IkeaReceivedNumbers,sb.IkeaReceivedNumber);	
+		} else if strings.ToLower(option) == "details" {
+			allDetails.IkeaReceived = append(allDetails.IkeaReceived,sb);	
+		}
+	}
+	
+	if strings.ToLower(option) == "ids" {
+		rabAsBytes, _ := json.Marshal(allIds)		
+		return shim.Success(rabAsBytes)	
+	} else if strings.ToLower(option) == "details" {
+		rabAsBytes, _ := json.Marshal(allDetails)
+		return shim.Success(rabAsBytes)	
+	}
+	
+	return shim.Success(nil)
+}
+
---
+++ SetupNetwork/chaincode/ikea/write_ledger.go
@@ -0,0 +1,103 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	pb "github.com/hyperledger/fabric/protos/peer"
+)
+
+//Create Ikea Received block
+func saveIkeaReceived(stub  shim.ChaincodeStubInterface, args []string) pb.Response {	
+	var err error
+	fmt.Println("Running saveIkeaReceived..")
+
+	if len(args) != 17 {
+		fmt.Println("Incorrect number of arguments. Expecting 17")
+		return shim.Error("Incorrect number of arguments. Expecting 17")
+	}
+
+	fmt.Println("Arguments :"+args[0]+","+args[1]+","+args[2]+","+args[3]+","+args[4]+","+args[5]+","+args[6]+","+args[7]+","+args[8]+","+args[9]+","+args[10]);
+
+	var bt IkeaReceived
+	bt.IkeaReceivedNumber				= args[0]
+	bt.IkeaStoreId						= args[1]
+	bt.PurchaseOrderNumber				= args[2]
+	bt.ConsignmentNumber				= args[3]	
+	bt.TransportConsitionSatisfied		= args[4]
+	bt.GUIDNumber						= args[5]
+	bt.MaterialName						= args[6]
+	bt.MaterialGrade					= args[7]	
+	bt.Quantity							= args[8]
+	bt.QuantityUnit						= args[9]	
+	bt.UsedByDate						= args[10]
+	bt.ReceivedDate						= args[11]
+	bt.TransitTime						= args[12]
+	bt.Storage							= args[13]
+	bt.UpdatedBy						= args[15]
+	bt.UpdatedOn						= args[16]
+
+	var acceptanceCriteria AcceptanceCriteria
+	
+	if args[14] != "" {
+		p := strings.Split(args[14], ",")
+		for i := range p {
+			c := strings.Split(p[i], "^")
+			acceptanceCriteria.Id 					= c[0]
+			acceptanceCriteria.RuleCondition 		= c[1]
+			acceptanceCriteria.ConditionSatisfied 	= c[2]
+			bt.AcceptanceCheckList	= append(bt.AcceptanceCheckList, acceptanceCriteria)
+		}
+	}
+
+	//Commit Inward entry to ledger
+	fmt.Println("saveIkeaReceived - Commit Ikea Received To Ledger");
+	btAsBytes, _ := json.Marshal(bt)
+	err = stub.PutState(bt.IkeaReceivedNumber, btAsBytes)
+	if err != nil {		
+		return shim.Error(err.Error())
+	}
+
+	//Update All Ikea ReceivedIds Array
+	allBAsBytes, err := stub.GetState("allIkeaReceivedIds")
+	if err != nil {
+		return shim.Error("Failed to get all Ikea Received Ids")
+	}
+	var allb AllIkeaReceivedIds
+	err = json.Unmarshal(allBAsBytes, &allb)
+	if err != nil {
+		return shim.Error("Failed to Unmarshal all Received")
+	}
+	allb.IkeaReceivedNumbers = append(allb.IkeaReceivedNumbers, bt.IkeaReceivedNumber)
+
+	allBuAsBytes, _ := json.Marshal(allb)
+	err = stub.PutState("allIkeaReceivedIds", allBuAsBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	return shim.Success(nil)
+}
+
+
